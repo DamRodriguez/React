5c5677d4448b5d4ea46a0888429ab6ec402bdb86
Damian Rodriguez <141194257+DamRodriguez@users.noreply.github.com>
2024-10-10 20:58:52 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
 import './App.css'
-import { TwitterCard } from './TwitterCard'
+import { TwitterCard } from './TwitterCard.jsx'
+
+const users = [
+    {
+        userName: 'damrod_',
+        name: 'Damián Rodríguez',
+        imgProfile: 'src/assets/img-profile/perro.jpeg',
+        isFollowing: true
+    },
+    {
+        userName: 'pedro123',
+        name: 'Pedro Alfonso',
+        imgProfile: 'src/assets/img-profile/gato.jpeg',
+        isFollowing: false
+    },
+    {
+        userName: 'juanCa90',
+        name: 'Juan Carlos',
+        imgProfile: 'src/assets/img-profile/sapo.jpg',
+        isFollowing: false
+    },
+    {
+        userName: 'hernesto4',
+        name: 'Hernesto Gutierrez',
+        imgProfile: 'src/assets/img-profile/pato.jpg',
+        isFollowing: true
+    }
+]
 
 export function App () {
 
     return (
         <section className='App'>
-            <TwitterCard userName='damrod_'>
-                Damián Rodríguez
-            </TwitterCard>
-            <TwitterCard userName='pedrolol1'>
-                Pedro Alfonso
-            </TwitterCard>
+            {
+                users.map(user => {
+                    const {userName, name, imgProfile, isFollowing} = user
+                    return (
+                        <TwitterCard
+                            key={userName}
+                            userName={userName}
+                            imgProfile={imgProfile}
+                            isFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterCard>
+                    )
+                })
+            }
         </section>
     )
 }
